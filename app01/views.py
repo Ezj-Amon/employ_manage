adc5a7bb742c1cd59a1fd7bc0b2620cb4684abf0
ameng <1070534263@qq.com>
2023-03-20 18:31:30 +0800

    更新modeform形式

--- app01/views.py
+++ app01/views.py
@@ -80,13 +80,37 @@ def user_add(request):
 class UserModelForm(forms.ModelForm):
     class Meta:
         model = models.UserInfo
-        fields = ["name", "password", "gender","age","account","create_time","depart"]
-        widgets={
-            "name":forms.TextInput(attrs={"class":"form-control"})
+        fields = ["name", "password", "age", 'account', 'create_time', "gender", "depart"]
+        # widgets = {
+        #     "name": forms.TextInput(attrs={"class": "form-control"}),
+        #     "password": forms.PasswordInput(attrs={"class": "form-control"}),
+        #     "age": forms.TextInput(attrs={"class": "form-control"}),
+        # }
 
-        }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 循环找到所有的插件，添加了class="form-control"
+        for name, field in self.fields.items():
+            # if name == "password":
+            #     continue
+            field.widget.attrs = {"class": "form-control", "placeholder": field.label}
+
 
 def user_modelform_add(request):
-    """ modelform版本的添加用户 """
-    form = UserModelForm()
-    return render(request, 'user_modelform_add.html', {'form': form})
+    """ 添加用户（ModelForm版本）"""
+    if request.method == "GET":
+        form = UserModelForm()
+        return render(request, 'user_modelform_add.html', {"form": form})
+
+    # 用户POST提交数据，数据校验。
+    form = UserModelForm(data=request.POST)
+    if form.is_valid():
+        # 如果数据合法，保存到数据库
+        # {'name': '123', 'password': '123', 'age': 11, 'account': Decimal('0'), 'create_time': datetime.datetime(2011, 11, 11, 0, 0, tzinfo=<UTC>), 'gender': 1, 'depart': <Department: IT运维部门>}
+        # print(form.cleaned_data)
+        # models.UserInfo.objects.create(..)
+        form.save()
+        return redirect('/user/list/')
+
+    # 校验失败（在页面上显示错误信息）
+    return render(request, 'user_modelform_add.html', {"form": form})
